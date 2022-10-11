938ba3a25542741f7b270bdae2501c31747a611f
Nick Phelps <nickphelps34@gmail.com>
2022-10-11 11:58:21 -0400

    commit tree 0614e45c8418406d5aa31c698189633754c818ed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+// write your code here
+const currentUser = "Nick Phelps";
+
+const welcomeMessage = "Welcome to Flatbook, " + "Nick Phelps!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "NICK PHELPS!"
+
+const shortGreeting = "Welcome, " + "N!"
